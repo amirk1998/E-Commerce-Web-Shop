0099141946517ac3ed98ac2cabcc4e2a446c71d9
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-05-11 11:50:25 +0430

    Navbar component has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Products from './components/Products/Products';
 import routes from './routes';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='App w-full h-screen bg-gray-100 px-8 py-4'>
+    <div className='App w-full h-full bg-gray-100 px-8 py-4'>
+      <Navbar />
       <Products />
       {/* <Routes>
             {routes.map((route) => {
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import logo from '../../assets/image/commerce.png';
+import { MdOutlineShoppingCart } from 'react-icons/md';
+
+const Navbar = () => {
+  const [count, setCount] = useState(54);
+  return (
+    <div className='h-16 mb-6 flex items-center px-4 bg-white w-full rounded-lg'>
+      <div className='flex items-center justify-between w-full pr-4'>
+        <div className='flex items-center'>
+          <img src={logo} alt='Commerce.js' className='h-6 w-6' />
+          <h2 className='ml-2 text-2xl'>Commerce.js</h2>
+        </div>
+
+        <button className='relative hover:bg-gray-100 hover:rounded-full p-4 '>
+          {count > 0 && (
+            <div className='absolute inline-flex items-center justify-center w-6 h-6 text-sm font-bold text-white bg-red-500 border-2 border-red-500 rounded-full -top-1 -right-1 '>
+              <span>{count}</span>
+            </div>
+          )}
+          <div className='text-slate-600 hover:text-slate-900'>
+            <MdOutlineShoppingCart
+              className='w-6 h-6  
+            '
+            />
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
