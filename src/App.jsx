3ea3b91e5a5768727ea8bc9d7bdc9563dfd9df6b
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-05-12 19:07:38 +0330

    BugFixed/handleRemoveFromCart has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   };
 
   const handleRemoveFromCart = async (productId) => {
-    const { cart } = await commerce.cart.delete(productId);
+    const { cart } = await commerce.cart.remove(productId);
     setCart(cart);
   };
 
